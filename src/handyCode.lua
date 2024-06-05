a72dc8bed73083bd873142646d07a8e743a64495
gabcasanova <con.casanovaproductions@outlook.com>
2024-06-05 20:14:21 -0300

    fun snippets library by myself

---
+++ src/handyCode.lua
@@ -0,0 +1,15 @@
+local handyCode = {}
+
+function handyCode.aabb(x1,y1,w1,h1, x2,y2,w2,h2)
+    -- Collision detection function;
+    -- Returns true if two boxes overlap, false if they don't;
+    -- x1,y1 are the top-left coords of the first box, while w1,h1 are its width and height;
+    -- x2,y2,w2 & h2 are the same, but for the second box.
+    return x1 < x2+w2 and
+           x2 < x1+w1 and
+           y1 < y2+h2 and
+           y2 < y1+h1
+    -- source: https://www.love2d.org/wiki/BoundingBox.lua
+end
+
+return handyCode

ad57e0bb3e9c577db538930797b2d9db73131d8b
gabcasanova <con.casanovaproductions@outlook.com>
2024-06-06 18:08:47 -0300

    moved mapui location

---
+++ src/mapView/mapUI.lua
@@ -0,0 +1,68 @@
+-- Import libraries.
+local Object = require("libs.classic")
+
+-- Import scripts.
+local Button = require ("src.ui.button")
+----------------------------------------
+
+local MapUI = Button:extend()
+
+function MapUI:new()
+    local MapView = _G.currentScene
+
+    -- Create buttons.
+    self.buttons = {
+        zoomIn = Button("Zoom In", 0, 0),
+        zoomOut = Button("Zoom Out", 0, 0)
+    }
+
+    -- Define button actions. --------------------------------
+    -- Zoom in button.
+    function self.buttons.zoomIn:customUpdate(dt)
+        local MapView = _G.currentScene
+
+        self.x = MapView.camera.winWidth - self.w - self.border
+        self.y = self.border
+    end
+    function self.buttons.zoomIn:leftAction()
+        local MapView = _G.currentScene
+
+        MapView.camera.scale = MapView.camera.scale + 0.5
+        print("aaa")
+    end
+
+    -- Zoom out button.
+    function self.buttons.zoomOut:customUpdate(dt)
+        local MapView = _G.currentScene
+
+        self.x = MapView.camera.winWidth - self.w - self.border
+        self.y = 50
+    end
+    function self.buttons.zoomOut:leftAction()
+        local MapView = _G.currentScene
+        MapView.camera.scale = MapView.camera.scale - 0.5
+    end
+end
+
+function MapUI:update(dt)
+    -- Update buttons.
+    for index, btn in pairs(self.buttons) do
+        btn:update(dt)
+    end
+end
+
+function MapUI:draw()
+    -- Draw buttons.
+    for index, btn in pairs(self.buttons) do
+        btn:draw()
+    end
+end
+
+function MapUI:mousepressed(x, y, button, istouch, presses)
+    -- Update button mouse action.
+    for index, btn in pairs(self.buttons) do
+        btn:mousepressed(x, y, button, istouch, presses)
+    end
+end
+
+return MapUI

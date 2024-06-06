379f08a04677e712a514d23d931dfc6007657cb9
gabcasanova <con.casanovaproductions@outlook.com>
2024-06-06 16:24:39 -0300

    added button interaction and renamed variable in for loop that shared name w/ function variable

--- src/ui/mapUI.lua
+++ src/ui/mapUI.lua
@@ -14,10 +14,15 @@ function MapUI:new()
     }
 
 
-    -- Define button functions.
+    -- Define button actions. --------------------------------
+    -- Zoom in button.
     function self.buttons.zoomIn:customUpdate(dt)
-        self.x = _G.gameCamera.winWidth - self.w - self.border
+        self.x = _G.mapCamera.winWidth - self.w - self.border
         self.y = self.border
+    end
+    function self.buttons.zoomIn:leftAction()
+        _G.mapCamera.scale = _G.mapCamera.scale + 1
+        print(_G.mapCamera.scale)
     end
 
 end
@@ -38,8 +43,8 @@ end
 
 function MapUI:mousepressed(x, y, button, istouch, presses)
     -- Update button mouse action.
-    for k, button in pairs(self.buttons) do
-        button:mousepressed(x, y, button, istouch, presses)
+    for k, buttons in pairs(self.buttons) do
+        buttons:mousepressed(x, y, button, istouch, presses)
     end
 end
 

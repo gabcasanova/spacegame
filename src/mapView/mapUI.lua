ef8b824019f90cc5d793540c32dbc2527ba7c579
gabcasanova <con.casanovaproductions@outlook.com>
2024-06-06 23:18:14 -0300

    added create building button and zoom scale debug info

--- src/mapView/mapUI.lua
+++ src/mapView/mapUI.lua
@@ -3,6 +3,7 @@ local Object = require("libs.classic")
 
 -- Import scripts.
 local Button = require ("src.ui.button")
+local Building = require("src.mapView.building")
 ----------------------------------------
 
 local MapUI = Button:extend()
@@ -13,33 +14,41 @@ function MapUI:new()
     -- Create buttons.
     self.buttons = {
         zoomIn = Button("Zoom In", 0, 0),
-        zoomOut = Button("Zoom Out", 0, 0)
+        zoomOut = Button("Zoom Out", 0, 0),
+        building = Button("Create building", 0, 0)
     }
 
     -- Define button actions. --------------------------------
     -- Zoom in button.
     function self.buttons.zoomIn:customUpdate(dt)
         local MapView = _G.currentScene
-
         self.x = MapView.camera.winWidth - self.w - self.border
         self.y = self.border
     end
     function self.buttons.zoomIn:leftAction()
         local MapView = _G.currentScene
-
         MapView.camera.scale = MapView.camera.scale + 0.5
     end
 
     -- Zoom out button.
     function self.buttons.zoomOut:customUpdate(dt)
         local MapView = _G.currentScene
-
         self.x = MapView.camera.winWidth - self.w - self.border
         self.y = 50
     end
     function self.buttons.zoomOut:leftAction()
         local MapView = _G.currentScene
         MapView.camera.scale = MapView.camera.scale - 0.5
+    end
+
+    -- Create building button.
+    function self.buttons.building:customUpdate(dt)
+        local MapView = _G.currentScene
+        self.x = MapView.camera.winWidth - self.w - self.border
+        self.y = 100
+    end
+    function self.buttons.building:leftAction()
+        --
     end
 end
 
@@ -57,12 +66,14 @@ function MapUI:draw()
     end
 
     -- Draw debug information.
+    local MapView = _G.currentScene
     local mX, mY = love.mouse.getPosition()
     if (_G.debug) then
         love.graphics.print(
             "DEBUG INFORMATION: \n\n" ..
             "Mouse X: " .. mX .. "\n" ..
-            "Mouse Y: " .. mY .. "\n", 
+            "Mouse Y: " .. mY .. "\n" ..
+            "Zoom scale: " .. MapView.camera.scale, 
         10, 10)
     end
 end

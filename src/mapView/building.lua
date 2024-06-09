805a526f408492373515feb6461df16a9e3ac544
gabcasanova <con.casanovaproductions@outlook.com>
2024-06-08 21:51:39 -0300

    Added more debug display options and rearanged mask code

--- src/mapView/building.lua
+++ src/mapView/building.lua
@@ -4,12 +4,16 @@
 
 -- Import libraries.
 local Object = require("libs.classic")
+
+-- Import Scripts.
+local handyCode = require("src.handyCode")
 ------------------------------------------------------------------
 
 local Building = Object:extend()
 
-function Building:new(x, y, spritesheet, spriteQuantity, spriteIndex)
+function Building:new(x, y, tw, th, spritesheet, spriteQuantity, spriteIndex)
     self.x, self.y = x, y
+    self.tileWidth, self.tileHeight = tw, th -- Tile size of the building in the grid.
 
     -- Set spritesheet. -------------------------------------------
     -- Default building sprite.
@@ -17,6 +21,7 @@ function Building:new(x, y, spritesheet, spriteQuantity, spriteIndex)
         self.spritesheet = _G.gameAsssets.graphics.isoCube
         self.spriteQuantity = 4
         self.spriteIndex = 1
+        self.tileWidth, self.tileHeight = 1, 1
     else
         self.spritesheet = spritesheet
         self.spriteQuantity = spriteQuantity
@@ -42,22 +47,70 @@ function Building:new(x, y, spritesheet, spriteQuantity, spriteIndex)
             self.spritesheet
         )
     end
+
+    -- Mouse collision mask.
+    -- The mask should be at the bottom of the sprite, a and a    
+    -- little bit than half of it's vertical and horizontal size.
+    self.maskX = self.x + self.spriteWidth/4
+    self.maskY = self.y+self.spriteHeight/2
+    self.maskW = self.spriteWidth/2-5             -- Currently, since my masks are a little bit bigger than they
+    self.maskH = self.spriteHeight/2-1.5          -- should be, i'm using some magic numbers to make them fit
+                                                  -- nicely. Magic numbers are NOT recommended and i shall redo
+                                                  -- for a more logical approach this once i have newer graphics!
     ---------------------------------------------------------------
+
+    -- Building variables.
+    self.isHovered = false
+    self.hoverDim = 0.8                           -- How dark the building will be when hovered.
 end
 
 function Building:update(dt)
-    --
+    -- Get the mouse position.
+    local mX, mY = love.mouse.getPosition()
+    local MapCamera = _G.currentScene.camera
+    local translatedX, translatedY = handyCode.mousePosToCamPos(mX, mY, MapCamera.x, MapCamera.y, MapCamera.scale)
+
+    -- Check if mouse is hovering the building.
+    if (handyCode.aabb(translatedX,translatedY,1,1, self.maskX, self.maskY, self.maskW, self.maskH)) then
+        self.isHovered = true
+    else
+        self.isHovered = false
+    end
 end
 
 function Building:draw()
-    -- Draw building.
-    love.graphics.draw(self.spritesheet, self.sprites[self.spriteIndex], self.x, self.y)
+    love.graphics.push("all")
+        -- Draw building.
+        if (self.isHovered) then -- Make sprite darker if cursor hovering tile.
+            love.graphics.setColor(self.hoverDim, self.hoverDim, self.hoverDim)
+        else                     -- Make sprite brighter if not.
+            love.graphics.setColor(1,1,1)
+        end
+        love.graphics.draw(self.spritesheet, self.sprites[self.spriteIndex], self.x, self.y)
 
-    -- Debug.
-    if (_G.debug) then
-        --love.graphics.rectangle("line", self.x, self.y, self.spriteWidth, self.spriteHeight)
-        --love.graphics.rectangle("fill", self.x, self.y, 5, 5)    
-    end
+        -- Debug.
+        if (_G.debug and self.isHovered) then
+            -- Sprite size.
+            love.graphics.setColor(0,0,0)
+            love.graphics.rectangle("line", self.x, self.y, self.spriteWidth, self.spriteHeight)
+
+            -- Position and size info.
+            love.graphics.setColor(1,1,1)
+            love.graphics.print(
+                "X: " .. self.x .. "\n" ..
+                "Y: " .. self.y .. "\n" ..
+                "Size: " .. self.tileWidth .. "," .. self.tileHeight,
+                self.x, self.y - 50
+            )
+
+            -- Mouse collision mask.
+            love.graphics.rectangle("line", self.maskX, self.maskY, self.maskW, self.maskH) 
+            
+            -- Sprite origin.
+            love.graphics.setColor(1,0,0)
+            love.graphics.rectangle("fill", self.x, self.y, 5, 5) 
+        end
+    love.graphics.pop()
 end
 
 return Building

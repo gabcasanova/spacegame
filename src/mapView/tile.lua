8b8ab694fdefd51863ba41bea4e0bc37bc84e7db
gabcasanova <con.casanovaproductions@outlook.com>
2024-06-09 21:10:01 -0300

    added spritesheets for tiles

--- src/mapView/tile.lua
+++ src/mapView/tile.lua
@@ -11,16 +11,17 @@ local handyCode = require("src.handyCode")
 
 local Tile = Object:extend()
 
-function Tile:new(terrain, x, y)
+function Tile:new(scene, terrain, x, y, tilewidth, tileheight, spritesheet, spriteQuantity, spriteIndex)
     self.x, self.y = x, y
-    self.tileWidth, self.tileHeight = 2, 2 -- Tile size in the grid.
+    self.tileWidth, self.tileHeight = tilewidth, tileheight -- Tile size in the grid.
 
     self.isHovered = false
     self.hoverDim = 0.8 -- How dark the tile will be when hovered.
 
+    -- TILE MASK -----------------------------------------------------------
     -- Tile border sprite.
-    -- Displays below the tile and is the graphic used 
-    -- as a base for mouse collision masks.
+    -- Displays below the tile and is the graphic used as a
+    -- base for mouse collision masks.
     self.tileSprite = _G.gameAsssets.graphics.tile
     self.tileSpriteWidth  = self.tileSprite:getWidth()
     self.tileSpriteHeight = self.tileSprite:getHeight()
@@ -28,8 +29,8 @@ function Tile:new(terrain, x, y)
     -- Mouse collision mask.
     -- The mask should be at the bottom of the sprite, a and a    
     -- little bit than half of it's vertical and horizontal size.
-    self.maskX = self.tileSpriteWidth/4
-    self.maskY = self.tileSpriteHeight/2
+    self.maskX = self.x + self.tileSpriteWidth/4
+    self.maskY = self.y + self.tileSpriteHeight/2
     self.maskW = self.tileSpriteWidth/2-5         -- Currently, since my masks are a little bit bigger than they
     self.maskH = self.tileSpriteHeight/2-1.5      -- should be, i'm using some magic numbers to make them fit
                                                   -- nicely. Magic numbers are NOT recommended.
@@ -43,12 +44,47 @@ function Tile:new(terrain, x, y)
     local mapOffsetY  = terrain.mapOffsetY
 
     self.masks = {}                               -- This works pretty much like the grid importer from
-    for row = 1, self.tileWidth, 1 do             -- terrain.lua.
+    for row = 1, self.tileWidth, 1 do             -- mapview/terrain.lua.
         for column = 1, self.tileHeight, 1 do
-            local desiredXPos, desiredYPos = handyCode.gridTileToIsometricTile(row, column, gridSize, tileOffsetX, tileOffsetY, mapOffsetX, mapOffsetY)
-            table.insert(self.masks, {x = self.maskX + desiredXPos, y = self.maskY + desiredYPos})
+            local desiredXPos, desiredYPos = handyCode.gridTileToIsometricTile(
+                row, column, gridSize, tileOffsetX, tileOffsetY, mapOffsetX, mapOffsetY
+            )
+            table.insert(self.masks, {x = self.maskX + desiredXPos, y = self.maskY})
+
+            print(self.maskY)
         end
     end
+    ------------------------------------------------------------------------
+
+    -- TILE SPRITESHEET ----------------------------------------------------
+    -- Check if tile has a building sprite.
+    if (spritesheet ~= nil) then
+        self.displaySprite = true
+        self.spritesheet = spritesheet
+        self.spriteQuantity = spriteQuantity
+        self.spriteIndex = spriteIndex
+
+        -- Get spritesheet size and set the sprite size based 
+        -- on the length of the spritehseet. This means that 
+        -- each sprite should have the same horizontal size.
+        self.spritesheetWidth = self.spritesheet:getWidth()
+        self.spritesheetHeight = self.spritesheet:getHeight()
+        self.spriteWidth = self.spritesheetWidth / self.spriteQuantity
+        self.spriteHeight = self.spritesheetHeight
+
+        -- Store in a table all the sprite positions as quads.
+        self.sprites = {}
+        for i = 0, self.spriteQuantity, 1 do
+            self.sprites[i+1] = love.graphics.newQuad(
+                self.spriteWidth * i, 
+                0, 
+                self.spriteWidth, 
+                self.spriteHeight, 
+                self.spritesheet
+            )
+        end
+    end
+    ------------------------------------------------------------------------
 end
 
 function Tile:update(scene, dt)
@@ -74,6 +110,11 @@ function Tile:draw(scene)
             love.graphics.setColor(1,1,1)
         end
         love.graphics.draw(self.tileSprite, self.x, self.y)
+
+        -- Draw building sprite.
+        if (self.displaySprite == true) then
+            love.graphics.draw(self.spritesheet, self.sprites[self.spriteIndex], self.x, self.y)
+        end
 
         -- Debug. 
         if (_G.debug and self.isHovered) then

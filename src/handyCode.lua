6382c648432df2c5c51f0880afcb8fce3b9e3524
gabcasanova <con.casanovaproductions@outlook.com>
2024-06-11 15:46:24 -0300

    code formatting

--- src/handyCode.lua
+++ src/handyCode.lua
@@ -5,7 +5,10 @@ local handyCode = {}
 -- x1,y1 are the top-left coords of the first box, while w1,h1 are its width and height;
 -- x2,y2,w2 & h2 are the same, but for the second box.
 function handyCode.aabb(x1, y1, w1, h1, x2, y2, w2, h2)
-    return x1 < x2 + w2 and x2 < x1 + w1 and y1 < y2 + h2 and y2 < y1 + h1
+    return x1 < x2 + w2 and 
+           x2 < x1 + w1 and 
+           y1 < y2 + h2 and
+           y2 < y1 + h1
     -- source: https://www.love2d.org/wiki/BoundingBox.lua
 end
 
